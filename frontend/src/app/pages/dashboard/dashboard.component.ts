aa9161105b86447a7eb6a96a456498015c1755fd
jaspreet04 <dhimanjaspreet04@gmail.com>
2022-05-22 13:02:06 -0700

    formatting

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,13 @@
 import { UserService } from './../../services/user.service';
 import onlineUser from 'src/app/model/onlineUser';
 import messages from 'src/app/model/messages';
-import { Component, OnInit, AfterViewChecked, ElementRef, ViewChild } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewChecked,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { SocketService } from 'src/app/services/socket.service';
 
 @Component({
@@ -9,12 +15,15 @@ import { SocketService } from 'src/app/services/socket.service';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit, AfterViewChecked{
+export class DashboardComponent implements OnInit, AfterViewChecked {
   @ViewChild('conversationScroll')
   private conversationScrollContainer!: ElementRef;
   public _onlineUsers: onlineUser[] = [];
   public conversation: messages[] = [];
-  constructor(private socketService: SocketService, private userService: UserService) {}
+  constructor(
+    private socketService: SocketService,
+    private userService: UserService
+  ) {}
   public message = '';
   private selectedUser!: string;
   ngOnInit(): void {
@@ -24,16 +33,14 @@ export class DashboardComponent implements OnInit, AfterViewChecked{
     this.socketService.OnupdateUsers().subscribe((data: any) => {
       this._onlineUsers = data;
     });
-    
 
     this.socketService.onUpdateMessage().subscribe((data: any) => {
       console.log(this._onlineUsers);
-      console.log(data.from +'=='+ this.selectedUser);
-      if(data.from == this.selectedUser)
-        this.conversation.push(data);
-      else{
-        this.incrementOrResetUreadMessages(false, data.from)
-        console.log(this._onlineUsers)
+      console.log(data.from + '==' + this.selectedUser);
+      if (data.from == this.selectedUser) this.conversation.push(data);
+      else {
+        this.incrementOrResetUreadMessages(false, data.from);
+        console.log(this._onlineUsers);
       }
     });
   }
@@ -49,26 +56,26 @@ export class DashboardComponent implements OnInit, AfterViewChecked{
     } catch (err) {}
   }
 
-  private incrementOrResetUreadMessages(reset: boolean, userid:string) {
-    var itemIndex = this._onlineUsers.findIndex(x => x.userId == userid);
+  private incrementOrResetUreadMessages(reset: boolean, userid: string) {
+    var itemIndex = this._onlineUsers.findIndex((x) => x.userId == userid);
     var item = this._onlineUsers[itemIndex];
-    if(reset)
-      item['unread'] = 0;
-    else
-      item['unread'] = (item.unread == null) ? 1 : item.unread+1;
-    this._onlineUsers[itemIndex]=item;
+    if (reset) item['unread'] = 0;
+    else item['unread'] = item.unread == null ? 1 : item.unread + 1;
+    this._onlineUsers[itemIndex] = item;
   }
 
   selectUser(userId: string) {
     this.selectedUser = userId;
-    this.incrementOrResetUreadMessages(true, userId)
+    this.incrementOrResetUreadMessages(true, userId);
     this.getPreviousConversation();
   }
 
-  private async getPreviousConversation(){
-     await this.userService.getUserConversation(this.selectedUser).subscribe((data) => {
-       this.conversation = data;
-     })
+  private async getPreviousConversation() {
+    await this.userService
+      .getUserConversation(this.selectedUser)
+      .subscribe((data) => {
+        this.conversation = data;
+      });
   }
 
   sendMessage() {
