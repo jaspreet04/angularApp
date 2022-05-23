c4c7942472171b7a893cf7a28e44090665db136e
jaspreet04 <dhimanjaspreet04@gmail.com>
2022-05-22 23:54:58 -0700

    authentication

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,11 +1,14 @@
 import { WebService } from './web.service';
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private webService: WebService) {}
+  constructor(private webService: WebService,
+    private cookieService: CookieService,
+    ) {}
 
   loginUser(email: string, password: string) {
     return this.webService.post('login', { email, password });
@@ -18,4 +21,9 @@ export class UserService {
   getUserConversation(userId:string) {
     return this.webService.post('dashboard/messages', { userId });
   }
+
+  getToken() {
+    return this.cookieService.get('token');
+  }
+
 }
